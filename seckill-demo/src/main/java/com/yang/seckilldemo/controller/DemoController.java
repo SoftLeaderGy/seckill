ad919db5db421cf5e0cb43251cc2354bb219f5d6
SoftLeaderGy <826044698@qq.com>
2022-03-22 16:32:49 +0800

    搭建项目

---
+++ seckill-demo/src/main/java/com/yang/seckilldemo/controller/DemoController.java
@@ -0,0 +1,22 @@
+package com.yang.seckilldemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @Description:
+ * @Author: Guo.Yang
+ * @Date: 2022/03/22/15:38
+ */
+@Controller
+@RequestMapping("/demo")
+public class DemoController {
+
+    @RequestMapping("/hello")
+    public String hello(Model model){
+        model.addAttribute("name","gy");
+        return "hello";
+    }
+}
+
