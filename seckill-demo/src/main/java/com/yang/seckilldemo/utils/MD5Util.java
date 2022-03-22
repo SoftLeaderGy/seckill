6c7a847642268af0c39b9b98435a9d0faf98a448
SoftLeaderGy <826044698@qq.com>
2022-03-22 16:55:32 +0800

    封装md5加密工具类

---
+++ seckill-demo/src/main/java/com/yang/seckilldemo/utils/MD5Util.java
@@ -0,0 +1,65 @@
+package com.yang.seckilldemo.utils;
+
+import org.apache.commons.codec.digest.DigestUtils;
+import org.springframework.stereotype.Component;
+
+/**
+ * @Description: MD5工具类
+ * @Author: Guo.Yang
+ * @Date: 2022/03/22/16:35
+ */
+@Component
+public class MD5Util {
+
+    // 加密的用的言， 加密密钥
+    private static final String salt = "1a2b3c4d";
+
+    public static String md5(String str){
+        return DigestUtils.md5Hex(str);
+    }
+
+    /**
+     * 第一次加密（页面输入后到服务端的加密）
+     * @param inputPass
+     * @return
+     */
+    public static String inputPassToFromPass(String inputPass){
+        String str = salt.charAt(0)+ salt.charAt(2)+ inputPass+ salt.charAt(5)+ salt.charAt(4);
+        return md5(str);
+    }
+
+    /**
+     * 第二次加密 （服务端到存入数据库的密码加密）
+     * @param formPass
+     * @param salt
+     * @return
+     */
+    public static String formPassToDBpass(String formPass,String salt){
+        String str = salt.charAt(0)+salt.charAt(2)+ formPass + salt.charAt(5)+ salt.charAt(4);
+        return md5(str);
+    }
+
+    /**
+     * 这个是直接从输入的密码  直接经过两次加密直接得到存入数据库的密码
+     * @param inputpass
+     * @param salt
+     * @return
+     */
+    public static String inputPassToDBPass(String inputpass , String salt){
+        String formPass = inputPassToFromPass(inputpass);
+        String dbPass = formPassToDBpass(formPass,salt);
+        return dbPass;
+    }
+
+
+    public static void main(String[] args) {
+        // ce21b747de5af71ab5c2e20ff0a60eea
+        System.out.println(inputPassToFromPass("123456"));
+
+        // 0687f9701bca74827fcefcd7e743d179
+        System.out.println(formPassToDBpass("ce21b747de5af71ab5c2e20ff0a60eea", "1a2b3c4d"));
+
+        // 0687f9701bca74827fcefcd7e743d179
+        System.out.println(inputPassToDBPass("123456", "1a2b3c4d"));
+    }
+}
