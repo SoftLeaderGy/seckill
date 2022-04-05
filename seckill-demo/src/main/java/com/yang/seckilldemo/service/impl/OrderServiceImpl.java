1bb3923e908b9e7475fe44f70462cced4492fc5c
SoftLeaderGy <826044698@qq.com>
2022-04-05 19:47:21 +0800

    优化--高并发时解决库存超卖的情况

--- seckill-demo/src/main/java/com/yang/seckilldemo/service/impl/OrderServiceImpl.java
+++ seckill-demo/src/main/java/com/yang/seckilldemo/service/impl/OrderServiceImpl.java
@@ -1,6 +1,7 @@
 package com.yang.seckilldemo.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.yang.seckilldemo.mapper.SeckillGoodsMapper;
 import com.yang.seckilldemo.mapper.SeckillOrderMapper;
@@ -11,6 +12,7 @@ import com.yang.seckilldemo.pojo.User;
 import com.yang.seckilldemo.service.GoodsService;
 import com.yang.seckilldemo.service.OrderService;
 import com.yang.seckilldemo.mapper.OrderMapper;
+import com.yang.seckilldemo.service.SeckillGoodsService;
 import com.yang.seckilldemo.utils.UUIDUtil;
 import com.yang.seckilldemo.vo.GoodsVo;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,15 +37,31 @@ implements OrderService{
     @Autowired
     private SeckillOrderMapper seckillOrderMapper;
     @Autowired
+    private SeckillGoodsService seckillGoodsService;
+    @Autowired
     private RedisTemplate redisTemplate;
     @Override
     public Order seckill(GoodsVo goodsVo, User user) {
+
+        // 高并发时会出现库存为负数的情况
+//        QueryWrapper<SeckillGoods> seckillGoodsQueryWrapper = new QueryWrapper<>();
+//        seckillGoodsQueryWrapper.eq("goods_id",goodsVo.getId());
+//        SeckillGoods seckillGoods = seckillGoodsMapper.selectOne(seckillGoodsQueryWrapper);
+//        seckillGoods.setStockCount(seckillGoods.getStockCount() - 1);
+//        seckillGoodsMapper.updateById(seckillGoods);
+
+
         QueryWrapper<SeckillGoods> seckillGoodsQueryWrapper = new QueryWrapper<>();
         seckillGoodsQueryWrapper.eq("goods_id",goodsVo.getId());
         SeckillGoods seckillGoods = seckillGoodsMapper.selectOne(seckillGoodsQueryWrapper);
-        seckillGoods.setStockCount(seckillGoods.getStockCount() - 1);
-        seckillGoodsMapper.updateById(seckillGoods);
+//        UpdateWrapper<SeckillGoods> seckillGoodsUpdateWrapper = new UpdateWrapper<SeckillGoods>();
+//        seckillGoodsUpdateWrapper.setSql("stock_count = stock_count -1").gt("stock_count", 0).eq("goods_id",goodsVo.getId());
+        int update = seckillGoodsMapper.update(seckillGoods, new UpdateWrapper<SeckillGoods>().setSql("stock_count = stock_count -1").gt("stock_count", 0).eq("goods_id", goodsVo.getId()));
 
+//        boolean update = seckillGoodsService.update(seckillGoodsUpdateWrapper);
+        if(update != 1){
+            return null;
+        }
         // 生成订单
         Order order = new Order();
         order.setUserId(user.getId());
